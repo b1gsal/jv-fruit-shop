4329cb334922babbb78ac5198c642a4210d737bc
Denya Basarab <denya2007basarab@gmail.com>
2026-02-17 20:17:18 +0200

    refactor: add null value validations

--- src/main/java/core/basesyntax/service/impl/ShopServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/ShopServiceImpl.java
@@ -2,6 +2,7 @@ package core.basesyntax.service.impl;
 
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.ShopService;
+import core.basesyntax.strategy.OperationHandler;
 import core.basesyntax.strategy.OperationStrategy;
 import java.util.List;
 
@@ -9,15 +10,24 @@ public class ShopServiceImpl implements ShopService {
     private final OperationStrategy operationStrategy;
 
     public ShopServiceImpl(OperationStrategy operationStrategy) {
+        if (operationStrategy == null) {
+            throw new RuntimeException("operationStrategy can't be null");
+        }
         this.operationStrategy = operationStrategy;
     }
 
     @Override
     public void process(List<FruitTransaction> transactions) {
+        if (transactions == null) {
+            throw new RuntimeException("transaction can't be null");
+        }
         for (FruitTransaction transaction : transactions) {
             FruitTransaction.Operation currentOperation = transaction.getOperation();
-            operationStrategy.getHandler(currentOperation)
-                    .handle(transaction.getFruit(), transaction.getQuantity());
+            OperationHandler handler = operationStrategy.getHandler(currentOperation);
+            if (handler == null) {
+                throw new RuntimeException("Can't find handler for operation: " + currentOperation);
+            }
+            handler.handle(transaction.getFruit(), transaction.getQuantity());
         }
     }
 }

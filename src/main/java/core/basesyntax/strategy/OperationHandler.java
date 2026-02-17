88589aad470211f02b76a86aba5b14505044a3e4
Denya Basarab <denya2007basarab@gmail.com>
2026-02-17 20:27:02 +0200

    feat: add default method for validation fruit and quantity

--- src/main/java/core/basesyntax/strategy/OperationHandler.java
+++ src/main/java/core/basesyntax/strategy/OperationHandler.java
@@ -2,4 +2,13 @@ package core.basesyntax.strategy;
 
 public interface OperationHandler {
     void handle(String fruit, int quantity);
+
+    default void validation(String fruit, int quantity) {
+        if (fruit == null) {
+            throw new RuntimeException("Fruit name can't be null");
+        }
+        if (quantity < 0) {
+            throw new RuntimeException("Quantity can't be negative");
+        }
+    }
 }

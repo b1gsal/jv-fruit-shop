85b5f0db897e6bf8ede368cd97aebd78fa32fcb7
Denya Basarab <denya2007basarab@gmail.com>
2026-02-17 19:49:16 +0200

    refactor: add null value validation

--- src/main/java/core/basesyntax/service/impl/ReaderServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReaderServiceImpl.java
@@ -9,10 +9,13 @@ import java.util.List;
 public class ReaderServiceImpl implements ReaderService {
     @Override
     public List<String> read(String filePath) {
+        if (filePath == null) {
+            throw new RuntimeException("Path to file can't be null");
+        }
         try {
             return Files.readAllLines(Path.of(filePath));
         } catch (IOException e) {
-            throw new RuntimeException("Can't get date from file " + filePath, e);
+            throw new RuntimeException("Can't get data from file " + filePath, e);
         }
     }
 }

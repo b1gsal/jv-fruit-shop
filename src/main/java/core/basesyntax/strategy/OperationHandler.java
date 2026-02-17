b35d3b5ba5bb7d83510321fc1f7bceb725ca1ac9
Denya Basarab <denya2007basarab@gmail.com>
2026-02-17 21:19:28 +0200

    refactor: add validation empty fruit name

--- src/main/java/core/basesyntax/strategy/OperationHandler.java
+++ src/main/java/core/basesyntax/strategy/OperationHandler.java
@@ -4,7 +4,7 @@ public interface OperationHandler {
     void handle(String fruit, int quantity);
 
     default void validation(String fruit, int quantity) {
-        if (fruit == null) {
+        if (fruit == null || fruit.trim().isEmpty()) {
             throw new RuntimeException("Fruit name can't be null");
         }
         if (quantity < 0) {

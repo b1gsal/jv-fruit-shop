9e578ef3c90c59cbdecf6e5a733126bfd6df4306
Denya Basarab <denya2007basarab@gmail.com>
2026-02-17 21:27:53 +0200

    refactor: add validation for handler and use copy of operations instead of original

--- src/main/java/core/basesyntax/strategy/impl/OperationStrategyImpl.java
+++ src/main/java/core/basesyntax/strategy/impl/OperationStrategyImpl.java
@@ -3,6 +3,7 @@ package core.basesyntax.strategy.impl;
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.strategy.OperationHandler;
 import core.basesyntax.strategy.OperationStrategy;
+import java.util.HashMap;
 import java.util.Map;
 
 public class OperationStrategyImpl implements OperationStrategy {
@@ -12,13 +13,17 @@ public class OperationStrategyImpl implements OperationStrategy {
         if (operations == null || operations.isEmpty()) {
             throw new RuntimeException("Operations map can't be null or empty");
         }
-        this.operations = operations;
+        this.operations = new HashMap<>(operations);
     }
 
     @Override
     public OperationHandler getHandler(FruitTransaction.Operation operation) {
         if (operation == null) {
             throw new RuntimeException("Operation can't be null");
+        }
+        OperationHandler handler = operations.get(operation);
+        if (handler == null) {
+            throw new RuntimeException("Can't find handler for operation: " + operation);
         }
         return operations.get(operation);
     }

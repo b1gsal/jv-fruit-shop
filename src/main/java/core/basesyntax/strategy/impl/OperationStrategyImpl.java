42e0699a891f0361c5081ecddb90c3da8fe007db
Denya Basarab <denya2007basarab@gmail.com>
2026-02-17 20:38:22 +0200

    refactor: add validation null values

--- src/main/java/core/basesyntax/strategy/impl/OperationStrategyImpl.java
+++ src/main/java/core/basesyntax/strategy/impl/OperationStrategyImpl.java
@@ -9,11 +9,17 @@ public class OperationStrategyImpl implements OperationStrategy {
     private final Map<FruitTransaction.Operation, OperationHandler> operations;
 
     public OperationStrategyImpl(Map<FruitTransaction.Operation, OperationHandler> operations) {
+        if (operations == null || operations.isEmpty()) {
+            throw new RuntimeException("Operations map can't be null or empty");
+        }
         this.operations = operations;
     }
 
     @Override
     public OperationHandler getHandler(FruitTransaction.Operation operation) {
+        if (operation == null) {
+            throw new RuntimeException("Operation can't be null");
+        }
         return operations.get(operation);
     }
 }

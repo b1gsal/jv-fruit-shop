03114e3c623c50eb8e9e13a5fb88d6e2513b4950
Denya Basarab <denya2007basarab@gmail.com>
2026-02-17 19:54:03 +0200

    refactor: add null value validation for report and path

--- src/main/java/core/basesyntax/service/impl/FileWriterServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/FileWriterServiceImpl.java
@@ -8,6 +8,12 @@ import java.nio.file.Path;
 public class FileWriterServiceImpl implements WriterService {
     @Override
     public void write(String report, String filePath) {
+        if (filePath == null) {
+            throw new RuntimeException("Path to file can't be null");
+        }
+        if (report == null || report.isEmpty()) {
+            throw new RuntimeException("Report string can't be null or empty");
+        }
         try {
             Files.write(Path.of(filePath), report.getBytes());
         } catch (IOException e) {

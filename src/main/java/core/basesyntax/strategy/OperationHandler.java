e5f616520e191644ded81c69541fbd075ded008f
Denya Basarab <denya2007basarab@gmail.com>
2026-02-19 21:11:12 +0200

    refactor: improve validation

--- src/main/java/core/basesyntax/strategy/OperationHandler.java
+++ src/main/java/core/basesyntax/strategy/OperationHandler.java
@@ -4,8 +4,8 @@ public interface OperationHandler {
     void handle(String fruit, int quantity);
 
     default void validation(String fruit, int quantity) {
-        if (fruit == null || fruit.trim().isEmpty()) {
-            throw new RuntimeException("Fruit name can't be null");
+        if (fruit == null || fruit.isBlank()) {
+            throw new RuntimeException("Fruit name can't be null or empty");
         }
         if (quantity < 0) {
             throw new RuntimeException("Quantity can't be negative");
